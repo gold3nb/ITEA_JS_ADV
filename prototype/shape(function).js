c3c14233df6dc6c06373f922991cd30d2be5c973
Vitalii <goldenbergtm@gmail.com>
2020-08-19 19:38:32 +0300

    Added function shape that return object

---
+++ prototype/shape(function).js
@@ -0,0 +1,51 @@
+function shape(...args) {
+	this.args = args;
+	return {
+		type: `${(this.args.length === 4) ? 'Rectangle'
+			: (this.args.length === 3) ? 'Triangle'
+			: (this.args.length > 4) ? `This figure has ${this.args.length} angles` 
+			: 'Incorrect figure'}`,
+
+		getP() {
+			if (this.type === 'Rectangle') {
+				return `P = ${args.reduce((value, resSumRect) => resSumRect += value)}`;
+			} else if (this.type === 'Triangle') {
+				return `P = ${args.reduce((value, resSumTrian) => resSumTrian += value)}`
+			} else {
+				return 'P = 0';
+			}
+		},
+
+		getS() {
+			if (this.type === 'Rectangle') {
+				return `S = ${args[0] * args[1]}`;
+			} else if (this.type === 'Triangle') {
+				return `S = ${1 / 2 * args[0] * args[1]}`;
+			} else {
+				return 'S = 0';
+			}
+		}
+	}
+}
+
+console.log(shape(2,4,6).type);
+console.log(shape(2,4,6).getP());
+console.log(shape(2,4,6).getS());
+
+console.log('-----------------');
+
+const result = shape(2,5,2,5);
+
+console.log(result.type);
+console.log(result.getP());
+console.log(result.getS());
+
+console.log('-----------------');
+
+const Triangle = function() {}
+Triangle.prototype = new shape(2,3,4);
+const triangle = new Triangle();
+
+console.log(triangle.type);
+console.log(triangle.getP());
+console.log(triangle.getS());
